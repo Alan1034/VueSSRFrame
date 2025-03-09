fed11cdd9fe2493d8f6489b59210d72761c88a48
Chendeli <419287484@qq.com>
2025-03-09 14:43:57 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,28 @@
-<template >
-<div>
+<!--
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2025-03-02 16:30:16
+ * @LastEditTime: 2025-03-09 14:43:31
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \VueSSRFrame\layouts\default.vue
+ * 
+-->
+<template>
+  <div id="default-layout">
     <p>Some default layout content shared across all pages</p>
     <slot />
   </div>
 </template>
 <script>
 export default {
-  
+
 }
 </script>
-<style lang="">
-  
+<style lang="less" scoped>
+#default-layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
