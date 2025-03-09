817febf164107815370839314dc375ef5b4d0222
Chendeli <419287484@qq.com>
2025-03-09 14:42:08 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
+/*
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2025-03-02 15:44:56
+ * @LastEditTime: 2025-03-09 14:41:23
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \VueSSRFrame\nuxt.config.ts
+ * 
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@element-plus/nuxt"],
+});
