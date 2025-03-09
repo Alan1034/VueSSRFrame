77c4d3b9210270058c5f6ec60339223bd5548988
Chendeli <419287484@qq.com>
2025-03-09 15:57:20 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2025-03-02 15:44:56
- * @LastEditTime: 2025-03-09 14:41:23
+ * @LastEditTime: 2025-03-09 15:57:03
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: 
@@ -13,4 +13,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@element-plus/nuxt"],
+  vite: {
+    optimizeDeps: {
+      force: true,
+    },
+  },
 });
